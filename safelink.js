83b3cfe3e355277d52629b1a9813148337cd385f
imansyahanis <50209929+imansyahanis@users.noreply.github.com>
2022-01-04 02:29:59 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="how-to-learn-forex-trading-with-5.html"}
 if(x=="2"){xxx="forex-trading-facts-you-need-to-know.html"}
 if(x=="3"){xxx="how-to-survive-and-succeed-in-forex.html"}
-if(x=="4"){xxx="how-to-prepare-oneself-to-be-pro-in.html"}a_to_ve[i].href="https://www.file.kangyusufmn.com/2021/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="how-to-prepare-oneself-to-be-pro-in.html"}a_to_ve[i].href="https://filesdownloadku.blogspot.com/2021/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
